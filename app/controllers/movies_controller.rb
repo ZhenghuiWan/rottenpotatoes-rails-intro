b503f8da07e1eb801dffa68195d7c285b143ecad
EC2 Default User <ec2-user@ip-172-31-38-92.us-east-2.compute.internal>
2019-02-20 17:51:57 +0000

    <Part2.6>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,22 +11,45 @@ class MoviesController < ApplicationController
   end
 
   def index
-		if params.key?(:sort_by)
-			session[:sort_by] = params[:sort_by]
-		elsif session.key?(:sort_by)
-			params[:sort_by] = session[:sort_by]
-			redirect_to movies_path(params) and return
-		end
-		@hilite = sort_by = session[:sort_by]
-		@all_ratings = Movie.all_ratings
-		if params.key?(:ratings)
-			session[:ratings] = params[:ratings]
-		elsif session.key?(:ratings)
-			params[:ratings] = session[:ratings]
-			redirect_to movies_path(params) and return
-		end
-		@checked_ratings = (session[:ratings].keys if session.key?(:ratings)) || @all_ratings
-    @movies = Movie.order(sort_by).where(rating: @checked_ratings)
+    @all_ratings = Movie.all_ratings
+
+    #Update seesion parameters: :sort_by :ratings
+    if !params[:sort_by].nil? and params[:sort_by] != session[:sort_by]
+      session[:sort_by] = params[:sort_by]
+    end
+
+    if !params[:ratings].nil? and params[:ratings] != session[:ratings]
+      session[:ratings] = params[:ratings]
+    end
+
+    #If no explicitly new sroting/filtering setting, redirect
+    if params[:ratings].nil? && params[:sort_by].nil?
+      #In case both session[:ratings] and sessioin[:sort_by] are nil, prevent infinite redirect loop
+      if !session[:ratings].nil? || !session[:sort_by].nil?
+        flash.keep
+        redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings])
+      end
+    end
+
+    @sort_by = session[:sort_by]
+    @ratings = session[:ratings]
+
+    #If no rating check_box checked, show all movies
+    #Initial value of @ratings shouldn't be nil. If it happens, set its value to @all_ratings
+    if(@ratings)
+      @rating_keys = @ratings.keys
+    else
+      @rating_keys = @all_ratings
+    end
+
+    #Select movies been checked
+    @movies = Movie.where(rating: @rating_keys)
+    #Sort movies 
+    if(@sort_by)
+      @movies = @movies.order(@sort_by)
+    else
+      @movies = @movies.all
+    end
   end
 
   def new
