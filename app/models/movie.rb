cfef589b9fd7907c991716743086bb9f7ae2a2f5
EC2 Default User <ec2-user@ip-172-31-38-92.us-east-2.compute.internal>
2019-02-20 17:55:33 +0000

    <Part2.7>

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,5 @@
 class Movie < ActiveRecord::Base
-	@@all_ratings = ['G','PG','PG-13','R']
-
-	def self.all_ratings
-		@@all_ratings
-	end
+  def self.all_ratings
+    ['G', 'PG', 'PG-13', 'R', 'NC-17']
+  end
 end
